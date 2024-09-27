4b1cf62302cb7632800346da7134b168332f6c63
bkounter <48039575+bkounter@users.noreply.github.com>
2024-09-26 20:50:07 -0500

    Good for now

---
+++ lua/config/appearance.lua
@@ -0,0 +1,79 @@
+-- Tokyonight setup
+require("tokyonight").setup({
+	style = "storm", -- Choose between "storm", "night", "day"
+	transparent = true, -- Enable transparency
+	terminal_colors = true, -- Apply the theme to terminal colors
+	styles = {
+		sidebars = "transparent",
+		floats = "transparent",
+	},
+	on_colors = function(colors)
+		-- Customize colors if necessary, or leave empty
+	end,
+	on_highlights = function(highlights, colors)
+		-- Customize highlights if necessary, or leave empty
+	end,
+})
+
+require("catppuccin").setup({
+	flavour = "auto", -- latte, frappe, macchiato, mocha
+	background = { -- :h background
+		light = "latte",
+		dark = "mocha",
+	},
+	transparent_background = true, -- disables setting the background color.
+	show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+	term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+	dim_inactive = {
+		enabled = false, -- dims the background color of inactive window
+		shade = "dark",
+		percentage = 0.15, -- percentage of the shade to apply to the inactive window
+	},
+	no_italic = false, -- Force no italic
+	no_bold = false, -- Force no bold
+	no_underline = false, -- Force no underline
+	styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+		comments = { "italic" }, -- Change the style of comments
+		conditionals = { "italic" },
+		loops = {},
+		functions = {},
+		keywords = {},
+		strings = {},
+		variables = {},
+		numbers = {},
+		booleans = {},
+		properties = {},
+		types = {},
+		operators = {},
+		-- miscs = {}, -- Uncomment to turn off hard-coded styles
+	},
+	color_overrides = {},
+	custom_highlights = {},
+	default_integrations = true,
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		nvimtree = true,
+		treesitter = true,
+		notify = false,
+		mini = {
+			enabled = true,
+			indentscope_color = "",
+		},
+		-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+	},
+})
+
+-- setup must be called before loading
+vim.cmd.colorscheme("catppuccin")
+
+-- -- Apply the colorscheme
+-- vim.cmd([[colorscheme tokyonight]])
+
+-- Transparency settings
+vim.cmd([[
+  hi Normal guibg=NONE ctermbg=NONE
+  hi NormalFloat guibg=NONE
+  hi LineNr guibg=NONE
+  hi SignColumn guibg=NONE
+]])
--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -21,24 +21,24 @@ return {
 	},
 
 	-- change trouble config
-	{
-		"folke/trouble.nvim",
-		-- opts will be merged with the parent spec
-		opts = { use_diagnostic_signs = true },
-	},
+	-- {
+	-- 	"folke/trouble.nvim",
+	-- 	-- opts will be merged with the parent spec
+	-- 	opts = { use_diagnostic_signs = true },
+	-- },
 
 	-- disable trouble
-	{ "folke/trouble.nvim", enabled = false },
+	-- { "folke/trouble.nvim", enabled = false },
 
 	-- override nvim-cmp and add cmp-emoji
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = { "hrsh7th/cmp-emoji" },
-		---@param opts cmp.ConfigSchema
-		opts = function(_, opts)
-			table.insert(opts.sources, { name = "emoji" })
-		end,
-	},
+	-- {
+	-- 	"hrsh7th/nvim-cmp",
+	-- 	dependencies = { "hrsh7th/cmp-emoji" },
+	-- 	---@param opts cmp.ConfigSchema
+	-- 	opts = function(_, opts)
+	-- 		table.insert(opts.sources, { name = "emoji" })
+	-- 	end,
+	-- },
 
 	-- change some telescope options and a keymap to browse plugin files
 	{
@@ -77,43 +77,43 @@ return {
 	},
 
 	-- add tsserver and setup with typescript.nvim instead of lspconfig
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"jose-elias-alvarez/typescript.nvim",
-			init = function()
-				require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-					vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-				end)
-			end,
-		},
-		---@class PluginLspOpts
-		opts = {
-			---@type lspconfig.options
-			servers = {
-				-- tsserver will be automatically installed with mason and loaded with lspconfig
-				tsserver = {},
-			},
-			-- you can do any additional lsp server setup here
-			-- return true if you don't want this server to be setup with lspconfig
-			---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-			setup = {
-				-- example to setup with typescript.nvim
-				tsserver = function(_, opts)
-					require("typescript").setup({ server = opts })
-					return true
-				end,
-				-- Specify * to use this function as a fallback for any server
-				-- ["*"] = function(server, opts) end,
-			},
-		},
-	},
+	-- {
+	-- 	"neovim/nvim-lspconfig",
+	-- 	dependencies = {
+	-- 		"jose-elias-alvarez/typescript.nvim",
+	-- 		init = function()
+	-- 			require("lazyvim.util").lsp.on_attach(function(_, buffer)
+	--          -- stylua: ignore
+	--          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+	-- 				vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+	-- 			end)
+	-- 		end,
+	-- 	},
+	-- 	---@class PluginLspOpts
+	-- 	opts = {
+	-- 		---@type lspconfig.options
+	-- 		servers = {
+	-- 			-- tsserver will be automatically installed with mason and loaded with lspconfig
+	-- 			tsserver = {},
+	-- 		},
+	-- 		-- you can do any additional lsp server setup here
+	-- 		-- return true if you don't want this server to be setup with lspconfig
+	-- 		---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+	-- 		setup = {
+	-- 			-- example to setup with typescript.nvim
+	-- 			tsserver = function(_, opts)
+	-- 				require("typescript").setup({ server = opts })
+	-- 				return true
+	-- 			end,
+	-- 			-- Specify * to use this function as a fallback for any server
+	-- 			-- ["*"] = function(server, opts) end,
+	-- 		},
+	-- 	},
+	-- },
 
 	-- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
 	-- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-	{ import = "lazyvim.plugins.extras.lang.typescript" },
+	-- { import = "lazyvim.plugins.extras.lang.typescript" },
 
 	-- add more treesitter parsers
 	{
